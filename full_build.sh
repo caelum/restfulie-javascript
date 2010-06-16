cae3c76086e2f220e87c8509fc51127fa4d0a6bf
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-16 15:04:46 -0300

    running tests on build process

--- full_build.sh
+++ full_build.sh
@@ -2,4 +2,6 @@ rvm gemset create restfulie-js
 rvm gemset use restfulie-js
 gem install rails -v="2.3.5" --no-ri --no-rdoc
 gem install restfulie -v="0.8.1"  --no-ri --no-rdoc
-gem install jspec
+gem install jspec --no-ri --no-rdoc
+
+sh tests.sh
