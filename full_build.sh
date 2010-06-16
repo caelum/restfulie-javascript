aabf173ba05aa0f1c109f40a61521e68440b1b7a
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-16 16:07:17 -0300

    removing commas

--- full_build.sh
+++ full_build.sh
@@ -1,6 +1,6 @@
 RVM_PATH="/home/hudson/.rvm/gems/ruby-1.8.7-head"
-PATH="$RVM_PATH/bin:/home/hudson/.rvm/bin:$PATH",
-RUBY_VERSION='ruby 1.8.7-head',
+PATH="$RVM_PATH/bin:/home/hudson/.rvm/bin:$PATH"
+RUBY_VERSION='ruby 1.8.7-head'
 GEM_HOME="$RVM_PATH"
 GEM_PATH="$RVM_PATH"
 BUNDLE_PATH="$RVM_PATH"
