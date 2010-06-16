1e1536623d0c35a8cfe482427884f6c852f04d0f
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-16 15:41:33 -0300

    one liner rocks

--- full_build.sh
+++ full_build.sh
@@ -1,10 +1,2 @@
-rvm use ruby-1.8.7-head
-
-rvm list
-
-rvm gemset create restfulie-js
-rvm gemset use restfulie-js
-gem install bundler --no-ri --no-rdoc
-bundle install
-
-sh tests.sh
+rvm install ruby-1.8.7-head
+rvm use ruby-1.8.7-head && rvm list && rvm gemset create restfulie-js && rvm gemset use restfulie-js && gem install bundler --no-ri --no-rdoc && bundle install && sh tests.sh
