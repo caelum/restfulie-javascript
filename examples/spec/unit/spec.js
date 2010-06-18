f260782318d4e5c60cb37889779af3bc5cbcd154
Carlos Alberto <euprogramador@gmail.com>
2010-06-18 09:13:01 -0300

    created a simple example to access the Twitter API

---
+++ examples/spec/unit/spec.js
@@ -0,0 +1,13 @@
+
+describe 'Restfulie-javascript examples'
+  describe 'accessing the Twitter API'
+    it 'recovering public_timeline'
+      timeline = Restfulie.at("http://api.twitter.com/1/statuses/public_timeline.xml").get();
+      timeline.response.body.should.not.equal ""
+      timeline.response.code.should.equal 200      
+      timeline.statuses.status[0].id.should.not.equal 0
+      timeline.statuses.status[0].text.should.not.equal ''
+    end
+
+  end
+end
